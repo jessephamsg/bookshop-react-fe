f386d1d3444a05fb01933c27d607fc84d4958c3f
Linawaty <linawatylim@gmail.com>
2020-08-04 22:48:03 +0800

    change the nav to dynamic category

--- src/components/general/navigation/BookCategory.js
+++ src/components/general/navigation/BookCategory.js
@@ -2,15 +2,14 @@ import React from 'react';
 import styles from './styles.module.css';
 
 
-export default function BookCategory () {
+export default function BookCategory (props) {
     return (
         <React.Fragment>
             <div id={styles.mainNavCategory}>Shop by category</div>
                 <div className={styles.categoryNav}>
-                    <li><a href="/cat/Fiction">Fiction</a></li>
-                    <li><a href="/cat/Non-Fiction">Non-Fiction</a></li>
-                    <li><a href="/cat/Children">Children</a></li>
-                    <li><a href="/cat/Science">Science</a></li>
+                    {props.categories.map(cat => {
+                        return (<li><a href={`/cat/${cat}`}>{cat}</a></li>)
+                    })}
                 </div>
         </React.Fragment>
     )
